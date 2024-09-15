4edf53d5216d13b58553e80da49198fd556d8886
Demi-Ajayi <demiajayi02@gmail.com>
2024-09-15 22:58:09 +0200

    Improved the UX

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ function generatePoem(event) {
     "You are a poem expert and love to write introspective and romantic yet concise poems. Your mission is to generate a poem 5 lines and shorter, separating each line by a <br>.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="glow">Generating poem...</div>`;
+
   axios.get(apiUrl).then(displayPoem);
 }
 
