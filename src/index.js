e3adb0340d805c8047c83e9f99779f0599abf6eb
Demi-Ajayi <demiajayi02@gmail.com>
2024-09-15 22:16:24 +0200

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Hello World",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let promptInput = document.querySelector("#user-prompt");
+  let apiKey = "b4bfef39d45aa0tc0o1538f819e3be9c";
+  let prompt = `Generate a poem about ${promptInput.value}`;
+  let context =
+    "You are a poem expert and love to write introspective and romantic yet concise poems. Your mission is to generate a poem 5 lines and shorter, separating each line by a <br>.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-form");
